7869e77669809ddfa57b5f66e54650d520a72d0b
VEERobo <71770036+VEERobo@users.noreply.github.com>
2021-04-10 18:37:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function preload(){
     song = loadSound("music.mp3");
 }
 function setup(){
-    canvas = createCanvas(600,500);
+    canvas = createCanvas(500,400);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
@@ -29,7 +29,7 @@ function gotPoses(results){
     }
 }
 function draw(){
-    image(video,0,0,600,500);
+    image(video,0,0,500,400);
 }
 function play(){
     song.play();
